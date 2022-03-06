a4ca97ccd9595f7ca012c428fba17c4439add5dc
sja <sirine.jabari@lesfurets.com>
2022-03-06 12:25:44 +0100

    My blabla app: build app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import './App.scss';
+import {Header} from "./components/Header/Header";
+import {Footer} from "./components/Footer/Footer";
+import {RideResutls} from "./components/RideResutls/RideResutls";
+import {createStore} from "redux";
+import reducer from "./store/reducer";
+import {fetchFailed, loadRides} from "./store/actions";
+import {Provider} from "react-redux";
+import {TripsApi} from "./api/TripsApi";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let store = createStore(reducer)
+
+
+    useEffect(() => {
+
+        TripsApi.getTrips()
+            .then((result: any) => {
+                if (!result.trips) {
+                    store.dispatch(fetchFailed(result.message ?? "A technical error occurred"))
+                } else {
+                    setTimeout(() => {
+
+                        store.dispatch(loadRides(result.trips ?? [], (result.search_info.count - result.search_info.full_trip_count), result.next_cursor))
+                    }, 2000)
+                }
+
+            }).catch((error: any) => {
+            console.log(error)
+        })
+
+    }, []);
+
+    return (
+        <div className="App">
+            <Header/>
+            <Provider store={store}>
+                <RideResutls/>
+            </Provider>
+            <Footer/>
+
+        </div>
+    );
 }
 
 export default App;
